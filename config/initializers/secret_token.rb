3724049a9e63a7d7e84ee8bc48de12840b5c242b
Laurici <lauriciiii@yahoo.com>
2012-12-03 16:51:38 +0200

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prjlaura::Application.config.secret_token = '7e9df816ca1afc710acb268269740ee8d6c6e39d30dd73ff73599e542240d7f8dc310474de0cfec6625163c0ef1bd9c7dfa062103de4f0c1d3884c13a7fc55aa'
